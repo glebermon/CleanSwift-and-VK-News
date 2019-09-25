5feb845327746152262c9cc5b4e1ae8313fe744b
Gleb <monetchikov.gleb@icloud.com>
2019-09-25 20:02:08 +0300

    refactor. optimised.

--- CreatingViews/ViewController2.swift
+++ CreatingViews/ViewController2.swift
@@ -17,7 +17,7 @@ class ViewController2: UIViewController {
         slider.addTarget(self, action: #selector(changeValue), for: .valueChanged)
         slider.value = 1
         slider.minimumValue = 1
-        slider.maximumValue = 3
+        slider.maximumValue = 6
         return slider
         
     }()
@@ -35,7 +35,7 @@ class ViewController2: UIViewController {
         
     }()
     
-    let containerView : UIView = {
+    let firstContainerView : UIView = {
         
         let contView : UIView = UIView()
         contView.translatesAutoresizingMaskIntoConstraints = false
@@ -43,10 +43,29 @@ class ViewController2: UIViewController {
         return contView
     }()
     
+    let secondContainerView : UIView = {
+            
+        let contView : UIView = UIView()
+        contView.translatesAutoresizingMaskIntoConstraints = false
+//           contView.backgroundColor = .red
+        return contView
+        }()
     
-    lazy var stackView : UIStackView = {
+    
+    lazy var firstStackView : UIStackView = {
         
         let sv = UIStackView(arrangedSubviews: [dice1])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.spacing = 20
+        sv.distribution = .fillEqually
+        sv.alignment = .center
+        return sv
+    }()
+    
+    lazy var secondStackView : UIStackView = {
+        
+        let sv = UIStackView(/*arrangedSubviews: [dice4]*/)
         sv.translatesAutoresizingMaskIntoConstraints = false
         sv.axis = .horizontal
         sv.spacing = 20
@@ -98,11 +117,13 @@ class ViewController2: UIViewController {
         dice3.image = UIImage(named: "dice1")
                 
         view.addSubview(pointOfWillSlider)
-        view.addSubview(containerView)
+        view.addSubview(firstContainerView)
+        view.addSubview(secondContainerView)
         view.addSubview(pointOfWillSlider)
         view.addSubview(pointOfWillTextView)
         view.addSubview(changeImageButton)
-        containerView.addSubview(stackView)
+        firstContainerView.addSubview(firstStackView)
+        secondContainerView.addSubview(secondStackView)
         
         pointOfWillTextView.text = "Current point of will: \(Int(pointOfWillSlider.value))"
 //        imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
@@ -121,8 +142,8 @@ class ViewController2: UIViewController {
         let topBarHeight = UIApplication.shared.statusBarFrame.size.height + (self.navigationController?.navigationBar.frame.height ?? 0.0)
         
         pointOfWillSlider.topAnchor.constraint(equalTo: view.topAnchor, constant: topBarHeight + 25).isActive = true
-        pointOfWillSlider.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
-        pointOfWillSlider.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        pointOfWillSlider.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        pointOfWillSlider.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
 //        pointOfWillSlider.widthAnchor.constraint(equalToConstant: calculateCubeSide()).isActive = true
 //        pointOfWillSlider.heightAnchor.constraint(equalToConstant: calculateCubeSide()).isActive = true
         
@@ -131,17 +152,27 @@ class ViewController2: UIViewController {
         pointOfWillTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
         
         
-        containerView.topAnchor.constraint(equalTo: pointOfWillTextView.bottomAnchor, constant: 20).isActive = true
-        containerView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
-        containerView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
-        containerView.heightAnchor.constraint(equalToConstant: calculateCubeSide()).isActive = true
+        firstContainerView.topAnchor.constraint(equalTo: pointOfWillTextView.bottomAnchor, constant: 20).isActive = true
+        firstContainerView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        firstContainerView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        firstContainerView.heightAnchor.constraint(equalToConstant: calculateCubeSide()).isActive = true
         
-        stackView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0).isActive = true
-        stackView.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: calculateCubeSide() / 3).isActive = true
-        stackView.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: -(calculateCubeSide() / 3)).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0).isActive = true
+        firstStackView.topAnchor.constraint(equalTo: firstContainerView.topAnchor, constant: 0).isActive = true
+        firstStackView.leftAnchor.constraint(equalTo: firstContainerView.leftAnchor, constant: calculateCubeSide() / 3.5).isActive = true
+        firstStackView.rightAnchor.constraint(equalTo: firstContainerView.rightAnchor, constant: -(calculateCubeSide() / 3.5)).isActive = true
+        firstStackView.bottomAnchor.constraint(equalTo: firstContainerView.bottomAnchor, constant: 0).isActive = true
         
-        changeImageButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 30).isActive = true
+        secondContainerView.topAnchor.constraint(equalTo: firstContainerView.bottomAnchor, constant: 20).isActive = true
+        secondContainerView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        secondContainerView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        secondContainerView.heightAnchor.constraint(equalToConstant: calculateCubeSide()).isActive = true
+        
+        secondStackView.topAnchor.constraint(equalTo: secondContainerView.topAnchor, constant: 0).isActive = true
+        secondStackView.leftAnchor.constraint(equalTo: secondContainerView.leftAnchor, constant: calculateCubeSide() / 3).isActive = true
+        secondStackView.rightAnchor.constraint(equalTo: secondContainerView.rightAnchor, constant: -(calculateCubeSide() / 3)).isActive = true
+        secondStackView.bottomAnchor.constraint(equalTo: secondContainerView.bottomAnchor, constant: 0).isActive = true
+        
+        changeImageButton.topAnchor.constraint(equalTo: secondContainerView.bottomAnchor, constant: 30).isActive = true
         changeImageButton.widthAnchor.constraint(equalToConstant: calculateCubeSide()).isActive = true
         changeImageButton.heightAnchor.constraint(equalToConstant: calculateCubeSide()/2).isActive = true
         changeImageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
@@ -157,19 +188,13 @@ class ViewController2: UIViewController {
         
         pointOfWillTextView.text = "Current point of will: \(Int(pointOfWillSlider.value))"
         
-        if sender.value >= 2 && sender.value < 3 && stackView.arrangedSubviews.count < 2 {
-            dice2.image = UIImage(named: "dice1")
-            stackView.addArrangedSubview(dice2)
-        } else if sender.value >= 2 && sender.value < 3 && stackView.arrangedSubviews.count > 2 {
-            stackView.removeArrangedSubview(dice3)
-            dice3.removeFromSuperview()
-        } else if sender.value >= 3 && stackView.arrangedSubviews.count < 3 {
-            dice3.image = UIImage(named: "dice1")
-            stackView.addArrangedSubview(dice3)
-        } else if sender.value < 2 && stackView.arrangedSubviews.count == 2 {
-            stackView.removeArrangedSubview(dice2)
-            dice2.removeFromSuperview()
-        }
+        updateNumberOfDice(sender.value)
+    }
+    
+    @objc func buttonAction(_ : UIButton) {
+        
+        rollDice()
+        
     }
     
     private func calculateCubeSide() -> CGFloat {
@@ -181,19 +206,57 @@ class ViewController2: UIViewController {
         }
     }
     
-    @objc func buttonAction(_ : UIButton) {
+    private func updateNumberOfDice(_ value : Float = 1) {
         
-        for case let imageView as UIImageView in stackView.arrangedSubviews {
-            diceIndex = Int(arc4random_uniform(6))
-            imageView.image = UIImage(named: diceImageArray[diceIndex])
+        if value >= 2 && value < 3 && firstStackView.arrangedSubviews.count < 2 {
+            dice2.image = UIImage(named: "dice1")
+            firstStackView.addArrangedSubview(dice2)
+        } else if value >= 2 && value < 3 && firstStackView.arrangedSubviews.count > 2 {
+            firstStackView.removeArrangedSubview(dice3)
+            dice3.removeFromSuperview()
+        } else if value >= 3 && firstStackView.arrangedSubviews.count < 3 {
+            dice3.image = UIImage(named: "dice1")
+            firstStackView.addArrangedSubview(dice3)
+        } else if value < 2 && firstStackView.arrangedSubviews.count == 2 {
+            firstStackView.removeArrangedSubview(dice2)
+            dice2.removeFromSuperview()
+        } else if value >= 4 && value < 5 && secondStackView.arrangedSubviews.count < 1 {
+            dice4.image = UIImage(named: "dice1")
+            secondStackView.addArrangedSubview(dice4)
+        } else if value < 4 && secondStackView.arrangedSubviews.count >= 1 {
+            secondStackView.removeArrangedSubview(dice4)
+            dice4.removeFromSuperview()
+        } else if value >= 5 && value < 6 && secondStackView.arrangedSubviews.count < 2 {
+            dice5.image = UIImage(named: "dice1")
+            secondStackView.addArrangedSubview(dice5)
+        } else if value < 5 && value >= 4 && secondStackView.arrangedSubviews.count >= 2 {
+            secondStackView.removeArrangedSubview(dice5)
+            dice5.removeFromSuperview()
         }
+        
     }
     
-    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        for case let imageView as UIImageView in stackView.arrangedSubviews {
+    private func rollDice() {
+        
+        for case let imageView as UIImageView in firstStackView.arrangedSubviews {
             diceIndex = Int(arc4random_uniform(6))
             imageView.image = UIImage(named: diceImageArray[diceIndex])
         }
+        
+        if secondStackView.arrangedSubviews.count > 0 {
+            for case let imageView as UIImageView in secondStackView.arrangedSubviews {
+                diceIndex = Int(arc4random_uniform(6))
+                imageView.image = UIImage(named: diceImageArray[diceIndex])
+            }
+        }
+        
+    }
+    
+
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDice()
+        
     }
 }
 
